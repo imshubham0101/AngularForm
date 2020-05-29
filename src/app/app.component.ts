4337c5748f7a9ed4dd8d30b4d562b17231acd80c
imshubham0101 <sfalke08@gmail.com>
2020-05-30 02:30:34 +0530

    3rd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f',{static:true}) signupForm : NgForm ;
+  defaultQuestion = 'pet' ;
+  answer = '' ;
+  genders = ['male' , 'female'] ;
+
+  user = {
+    username : '' ,
+    email : '',
+    secretQuestion : '',
+    answer : '',
+    gender : ''
+  }
+
+  submitted : boolean = false ;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
@@ -14,6 +28,20 @@ export class AppComponent {
   // onSubmit(form : NgForm){
   //   console.log(form);
   // }
+  
+  onSubmit(){
+    console.log(this.signupForm);
+    this.submitted = true ;
+    this.user.username = this.signupForm.value.userInfo.username;
+    this.user.email = this.signupForm.value.userInfo.email ;
+    this.user.secretQuestion = this.signupForm.value.secret ;
+    this.user.answer = this.signupForm.value.questionAnswer ;
+    this.user.gender = this.signupForm.value.gender ;
+
+
+    this.signupForm.reset();
+  }
+  
 
   
 }
